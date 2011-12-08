a7737000572e47c24b91f0d9b0132ec379c77c5f
Andrea Salvadore <andrea.salvadore@gmail.com>
2011-12-08 01:18:00 +0000

    refactored the notifications to record failures

--- app/models/auditlog/AuditLogEvent.java
+++ app/models/auditlog/AuditLogEvent.java
@@ -42,4 +42,99 @@ public class AuditLogEvent extends Model {
     	return this.model+":"+this.modelId + ":" + this.property + ":" + this.operation;
     }
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((actor == null) ? 0 : actor.hashCode());
+		result = prime * result
+				+ ((createdAt == null) ? 0 : createdAt.hashCode());
+		result = prime * result + ((model == null) ? 0 : model.hashCode());
+		result = prime * result + ((modelId == null) ? 0 : modelId.hashCode());
+		result = prime * result
+				+ ((newValue == null) ? 0 : newValue.hashCode());
+		result = prime * result
+				+ ((oldValue == null) ? 0 : oldValue.hashCode());
+		result = prime * result
+				+ ((operation == null) ? 0 : operation.hashCode());
+		result = prime * result
+				+ ((property == null) ? 0 : property.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (!(obj instanceof AuditLogEvent)) {
+			return false;
+		}
+		AuditLogEvent other = (AuditLogEvent) obj;
+		if (actor == null) {
+			if (other.actor != null) {
+				return false;
+			}
+		} else if (!actor.equals(other.actor)) {
+			return false;
+		}
+		if (createdAt == null) {
+			if (other.createdAt != null) {
+				return false;
+			}
+		} else if (!createdAt.equals(other.createdAt)) {
+			return false;
+		}
+		if (model == null) {
+			if (other.model != null) {
+				return false;
+			}
+		} else if (!model.equals(other.model)) {
+			return false;
+		}
+		if (modelId == null) {
+			if (other.modelId != null) {
+				return false;
+			}
+		} else if (!modelId.equals(other.modelId)) {
+			return false;
+		}
+		if (newValue == null) {
+			if (other.newValue != null) {
+				return false;
+			}
+		} else if (!newValue.equals(other.newValue)) {
+			return false;
+		}
+		if (oldValue == null) {
+			if (other.oldValue != null) {
+				return false;
+			}
+		} else if (!oldValue.equals(other.oldValue)) {
+			return false;
+		}
+		if (operation != other.operation) {
+			return false;
+		}
+		if (property == null) {
+			if (other.property != null) {
+				return false;
+			}
+		} else if (!property.equals(other.property)) {
+			return false;
+		}
+		return true;
+	}
+    
+    
+
 }
